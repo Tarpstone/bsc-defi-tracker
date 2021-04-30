833d92c65ab3b10d302bd60069a3e7924253d549
AnnuityDew <annuitydew@gmail.com>
2021-04-29 22:27:08 -0400

    Building out the logic

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import csv
 from time import sleep
 from bscscan import BscScan
 import pandas as pd
@@ -7,116 +8,129 @@ import httpx
 API_SLEEP_TIMER = 0.5
 
 
-def portfolio():
-    # data read for API keys and wallet addresses
-    bsc_api = open("data/BSCAPIKEY.txt", "r")
-    cmc_api = open("data/CMCAPIKEY.txt", "r")
-    wallets = open("data/WALLETS.txt", "r")
+def bsc_defi_tracker():
+    # data read for API keys, wallet addresses, and LP farms
+    with open("data/BSCAPIKEY.csv", "r") as bsc_api:
+        BSC_API_KEY = bsc_api.readline()
 
-    BSC_API_KEY = bsc_api.readline()
-    CMC_API_KEY = cmc_api.readline()
-    ADDRESSES = [line.strip() for line in wallets]
+    with open("data/CMCAPIKEY.csv", "r") as cmc_api:
+        CMC_API_KEY = cmc_api.readline()
+
+    with open("data/WALLETS.csv", "r") as wallets:
+        ADDRESSES = [line.strip() for line in wallets]
+
+    # for reading farms, we need to split the underlying token addresses into a list
+    farms = pd.read_csv("data/CONTRACTS.csv")
+    farms["underlying_token_addresses"] = farms["underlying_token_addresses"].str.split(":")
 
     # initialize bsc python client
     bsc = BscScan(BSC_API_KEY)
 
     # first, generate a unique list of every contractAddress the wallet has interacted with.
-    # we'll do this using the full transaction list
-    portfolio_df = pd.DataFrame()
+    # we'll do this using the full token transaction list.
+    # normal and internal transactions aren't necessary (think spending approvals, etc.)
+    transaction_df = pd.DataFrame()
     for address in ADDRESSES:
         sleep(API_SLEEP_TIMER)
-        portfolio = bsc.get_bep20_token_transfer_events_by_address(
+        transactions = bsc.get_bep20_token_transfer_events_by_address(
             address, startblock=1, endblock=9999999, sort="asc"
         )
-        if portfolio_df.empty:
-            portfolio_df = pd.DataFrame(portfolio)
-        else:
-            portfolio_df = pd.concat([portfolio_df, pd.DataFrame(portfolio)])
+        transaction_df = pd.DataFrame(transactions)
 
-    wallet_df = (
-        portfolio_df[["contractAddress", "tokenName", "tokenSymbol", "tokenDecimal"]]
-        .drop_duplicates(subset=["contractAddress"])
-        .set_index("contractAddress")
-    )
-    unique_contracts = wallet_df.index.tolist()
+        # need an emergency rename of the "from" field here. this is a Python keyword.
+        # if we try to iterate over namedtuples, "from" will become "_6". investigate =]
+        transaction_df.rename(columns={"from": "source"}, inplace=True)
 
-    # symbol list for CMC. remove known unsupported values for now
-    unsupported_tokens = [
-        "mooAutoCAKE-BNB",
-        "mooCakeSmart",
-        "mooSwampyCAKE-BNB",
-        "sBGO",
-        "SYRUP",
-        "TOAD",
-        "mooPancakeLINK-BNB",
-        "mooPancakeBAND-BNB",
-        "mooPancakeWATCH-BNB",
-        "mooPancakeODDZ-BNB",
-        "mooCakeV2ODDZ-BNB",
-        "mooCakeV2COS-BNB",
-        "mooCakeV2LTO-BNB",
-        "mooCakeV2BAND-BNB",
-        "mooCakeV2LINK-BNB",
-        "MDEX LP",
-        "BUSD-T",
-    ]
-    symbol_list = list(wallet_df.tokenSymbol.unique())
-    for token in unsupported_tokens:
-        if token in symbol_list:
-            symbol_list.remove(token)
-
-    # start token balances at zero
-    wallet_df["api_balance"] = 0
-
-    # now we'll get the account balance for each of these
-    # (sleeping for half a second so we don't overload the API)
-    for contract in unique_contracts:
-        sleep(API_SLEEP_TIMER)
-        wallet_df.loc[
-            contract, "api_balance"
-        ] = bsc.get_acc_balance_by_token_contract_address(contract, ADDRESSES[0])
-
-    # data type cleanup
-    wallet_df = data_typer(wallet_df)
-
-    # actual token balance
-    wallet_df["tokenFactor"] = 10
-    wallet_df["balance"] = wallet_df["api_balance"] / (
-        wallet_df["tokenFactor"] ** wallet_df["tokenDecimal"]
-    )
-
-    # now we need prices.
-    with httpx.Client() as client:
-        headers = {
-            "Accepts": "application/json",
-            "X-CMC_PRO_API_KEY": CMC_API_KEY,
-        }
-
-        # this call will grab the map of CMC ID to each crypto, but I think we can just use symbol.
-        # r = client.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/map", headers=headers)
-        # cmc_df = pd.DataFrame(r.json().get("data"))
-        symbol_string = ",".join(symbol_list)
-        r = client.get(
-            f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol={symbol_string}",
-            headers=headers,
+        wallet_df = (
+            transaction_df[["contractAddress", "tokenName", "tokenSymbol", "tokenDecimal"]]
+            .drop_duplicates(subset=["contractAddress"])
+            .set_index("contractAddress")
         )
-        cmc_df = pd.DataFrame(r.json().get("data")).transpose()
-        expanded_quote_json = cmc_df.quote.apply(json_to_pd_series)
+        unique_contracts = wallet_df.index.tolist()
 
-        # axis=1 expands the JSON field horizontally. very cool
-        cmc_df = pd.concat([cmc_df, expanded_quote_json], axis=1)
+        # iterate over transaction_df and make necessary updates to wallet/farms accordingly
+        for row in transaction_df.itertuples():
+            # first copy the namedtuple to dict so we can mutate it
+            row_dict = row._asdict()
+            print(row_dict['source'])
 
-        print("test")
+        # symbol list for CMC. remove known unsupported values for now
+        unsupported_tokens = [
+            "mooAutoCAKE-BNB",
+            "mooCakeSmart",
+            "mooSwampyCAKE-BNB",
+            "sBGO",
+            "SYRUP",
+            "TOAD",
+            "mooPancakeLINK-BNB",
+            "mooPancakeBAND-BNB",
+            "mooPancakeWATCH-BNB",
+            "mooPancakeODDZ-BNB",
+            "mooCakeV2ODDZ-BNB",
+            "mooCakeV2COS-BNB",
+            "mooCakeV2LTO-BNB",
+            "mooCakeV2BAND-BNB",
+            "mooCakeV2LINK-BNB",
+            "MDEX LP",
+            "BUSD-T",
+        ]
+        symbol_list = list(wallet_df.tokenSymbol.unique())
+        for token in unsupported_tokens:
+            if token in symbol_list:
+                symbol_list.remove(token)
 
-    # join price info back to main dataset
-    wallet_df = wallet_df.merge(
-        cmc_df[["symbol", "price"]],
-        left_on="tokenSymbol",
-        right_on="symbol",
-        how="left",
-    )
-    wallet_df["value"] = wallet_df["balance"] * wallet_df["price"]
-    wallet_df.to_csv("output/portfolio.csv")
+        # start token balances at zero
+        wallet_df["api_balance"] = 0
+
+        # now we'll get the account balance for each of these
+        # (sleeping for half a second so we don't overload the API)
+        for contract in unique_contracts:
+            sleep(API_SLEEP_TIMER)
+            wallet_df.loc[
+                contract, "api_balance"
+            ] = bsc.get_acc_balance_by_token_contract_address(contract, ADDRESSES[0])
+
+        # data type cleanup
+        wallet_df = data_typer(wallet_df)
+
+        # actual token balance
+        wallet_df["tokenFactor"] = 10
+        wallet_df["balance"] = wallet_df["api_balance"] / (
+            wallet_df["tokenFactor"] ** wallet_df["tokenDecimal"]
+        )
+
+        # now we need prices.
+        with httpx.Client() as client:
+            headers = {
+                "Accepts": "application/json",
+                "X-CMC_PRO_API_KEY": CMC_API_KEY,
+            }
+
+            # this call will grab the map of CMC ID to each crypto, but I think we can just use symbol.
+            # r = client.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/map", headers=headers)
+            # cmc_df = pd.DataFrame(r.json().get("data"))
+            symbol_string = ",".join(symbol_list)
+            r = client.get(
+                f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol={symbol_string}",
+                headers=headers,
+            )
+            cmc_df = pd.DataFrame(r.json().get("data")).transpose()
+            expanded_quote_json = cmc_df.quote.apply(json_to_pd_series)
+
+            # axis=1 expands the JSON field horizontally. very cool
+            cmc_df = pd.concat([cmc_df, expanded_quote_json], axis=1)
+
+            print("test")
+
+        # join price info back to main dataset
+        wallet_df = wallet_df.merge(
+            cmc_df[["symbol", "price"]],
+            left_on="tokenSymbol",
+            right_on="symbol",
+            how="left",
+        )
+        wallet_df["value"] = wallet_df["balance"] * wallet_df["price"]
+        wallet_df.to_csv("output/portfolio.csv")
 
     # Cake-LPs are something we'll have to track ourselves using the contract address and the assets that go into the transaction.
     return
@@ -162,4 +176,4 @@ def json_to_pd_series(json_dict):
 
 
 if __name__ == "__main__":
-    portfolio()
+    bsc_defi_tracker()
